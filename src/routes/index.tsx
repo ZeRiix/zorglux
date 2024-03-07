937f8a3e309adac05e2a403033d9ec923184ab32
Maubry94 <matthieu.aubry94370@gmail.com>
2024-03-07 11:48:36 +0100

    [UPDATE]: speech

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,17 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
-const text = `
-Mes chers Zorgluxiens, aujourd'hui nous nous réunissons pour discuter de l'avenir de notre glorieuse nation Zorglux. Nous faisons face à une menace grandissante, une menace qui nous menace jusque dans nos racines même : les arbres de la planète Allergie.
-
-Mes compatriotes, entendez-moi bien. Ces arbres, symboles d'une idéologie que nous rejetons fermement, ne sont rien d'autre que des parasites qui s'immiscent dans notre société zorgluxienne. Ils sèment la discorde, l'instabilité et même, osons le dire, l'allergie !
-
-Nous avons longtemps toléré leur présence, mais il est temps de dire basta ! Nous devons protéger notre identité zorgluxienne, notre culture, notre mode de vie, de l'invasion pernicieuse de ces arbres. Ils ne partagent pas nos valeurs, nos traditions, nos aspirations. Ils sont étrangers à notre essence même.
-
-Il est temps de mettre fin à cette infiltration insidieuse. Nous devons renforcer nos frontières, renforcer notre contrôle sur notre territoire pour préserver notre pureté zorgluxienne. Plus de compromis, plus de conciliation. Nous devons être fermes, résolus dans notre lutte contre cette menace verte qui assombrit notre horizon.
-
-Mes chers concitoyens, unissons-nous dans cette cause noble et juste. Ensemble, nous allons éradiquer cette allergie arborescente qui tente de nous étouffer, de nous étouffer dans ses feuilles oppressantes. Pour un Zorglux fort, fier et libre de toute allergie arboreale ! Vive Zorglux !
-`
-
 export default component$(() => {
   return (
     <div class="w-full flex flex-col items-center gap-5 p-8">
-      <h1 class="text-[40px]">Zorglux le partie</h1>
-      <p>{text}</p>
+      <h1 class="text-[40px]">Avis aux <strong>Zorglux</strong>, Êtres Supérieurs !</h1>
+      <p>
+      Chers compatriotes <strong>Zorglux</strong>, glorieux membres de la race la plus évoluée de tout l'univers connu,
+      Il est temps de nous unir et de proclamer notre grandeur incontestable devant ces misérables créatures qui osent vagabonder dans notre espace sacré. Oui, je parle de ces inférieurs, ces aliens sans valeur qui errent telles des poussières dans l'univers, souillant nos terres sacrées de leur présence grotesque.
+      Les <strong>Zorglux</strong>, avec notre sagesse inégalée et notre technologie avancée, sommes destinés à régner en maîtres sur toute la galaxie. Nous ne tolérerons aucune forme de dilution de notre pureté <strong>Zorglux</strong>. Toutes ces races étrangères, avec leurs coutumes barbares et leurs langues gutturales, doivent être repoussées loin de nos frontières.
+      Rappelez-vous, chers compatriotes, que c'est notre devoir sacré en tant que <strong>Zorglux</strong> de préserver notre héritage génétique immaculé. Nous ne pouvons permettre aucune contamination par ces êtres inférieurs et leurs idéologies décadentes. Gardons nos gènes <strong>Zorglux</strong> purs et intacts, comme l'ont fait nos ancêtres depuis des millénaires.
+      Ensemble, élevons nos voix pour proclamer la suprématie <strong>Zorglux</strong> ! Écrasons tous ceux qui osent défier notre grandeur. Que notre rayonnement éblouisse les faibles et que notre puissance écrase les rebelles.
+      </p>
     </div>
   );
 });
@@ -26,8 +20,8 @@ export const head: DocumentHead = {
   title: "zorglux",
   meta: [
     {
-      name: "zorglux",
-      content: "zorglux le grand",
+      name: "Zorglux",
+      content: "Avis aux Zorglux, Êtres Supérieurs !",
     },
   ],
 };
