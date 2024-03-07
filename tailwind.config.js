dc6770415898a57e3862dd04e8d7c44978c9e917
zeriix <wflorentin@myges.fr>
2024-03-07 12:35:21 +0100

    - [ADD]: Adding tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./ressources/**/*.{js,vue}", "./ressources/**/*.blade.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
