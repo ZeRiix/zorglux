0f0f6aa8db38dc2d26dbde7cfa3639d4be72eaee
Maubry94 <matthieu.aubry94370@gmail.com>
2024-03-08 18:56:46 +0100

    [FIX]: images path

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -19,9 +19,9 @@ export default component$(() => {
   return (
     <div class="relative">
       <div class="space absolute -z-50 w-full h-full bg-black">
-        <div class="stars absolute -z-40 w-full h-full bg-[url('/public/images/stars.webp')] bg-repeat bg-center"></div>
-        <div class="twinkling absolute -z-30 w-full h-full bg-[url('/public/images/twinkling.webp')] bg-repeat bg-center"></div>
-        <div class="clouds absolute -z-20 w-full h-full bg-[url('/public/images/clouds.webp')] bg-repeat bg-center"></div>
+        <div class="stars absolute -z-40 w-full h-full bg-[url('/images/stars.webp')] bg-repeat bg-center"></div>
+        <div class="twinkling absolute -z-30 w-full h-full bg-[url('/images/twinkling.webp')] bg-repeat bg-center"></div>
+        <div class="clouds absolute -z-20 w-full h-full bg-[url('/images/clouds.webp')] bg-repeat bg-center"></div>
       </div>
       <Header />
       <main class="py-12 min-h-screen flex flex-col items-center">
