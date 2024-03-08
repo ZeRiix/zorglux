ceefee0b33eac632aae35c06c16a4db4a37812df
Maubry94 <matthieu.aubry94370@gmail.com>
2024-03-08 11:40:39 +0100

    [ADD]: star wars generic (to optimize)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,16 @@ export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      fontFamily: {
+        'news-cycle': ['News Cycle', 'sans-serif']
+      },
       colors: {
         darkerblue: "#2f4550",
         darkblue: "#586f7c",
         blue: "#006cb0",
-        skyblue: "#b8dbd9",
+        skyblue: "#4bd5ee",
         darkwhite: "#f4f4f9",
+        yellow: "#ffd700"
       }
     },
   },
