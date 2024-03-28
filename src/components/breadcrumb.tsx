969f5f6d9695a81b33a88b5a9f4d9a1d33c01bff
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2024-03-28 10:17:38 +0000

    [UPDATE]: breadcrumb

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -9,11 +9,18 @@ export default component$((props: BreadcrumbProps) => {
         <ol class="mb-4 flex items-center whitespace-nowrap" aria-label="Breadcrumb">
             {props.labels.map((label, index) => (
                 <li key={index} class="inline-flex items-center">
-                    <a class={["flex items-center text-sm hover:text-blue focus:outline-none focus:text-blue", index === props.labels.length - 1 ? 'text-white' : 'text-grey']} href={label.url}>
-                        {label.label}
-                    </a>
                     {index < props.labels.length - 1 && (
-                        <svg class="flex-shrink-0 mx-2 overflow-visible size-4 text-grey" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6" /></svg>
+                        <a class="flex items-center text-sm text-grey hover:text-blue focus:outline-none focus:text-blue" href={label.url}>
+                            {label.label}
+                        </a>
+                    )}
+                    {index === props.labels.length - 1 && (
+                        <span class="flex items-center text-sm text-white">
+                            {label.label}
+                        </span>
+                    )}
+                    {index < props.labels.length - 1 && (
+                        <span class="flex-shrink-0 mx-2 overflow-visible text-grey">{">"}</span>
                     )}
                 </li>
             ))}
