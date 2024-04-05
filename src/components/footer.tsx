a5e6fd52c94c17e0a30eaed1c7136727e89ae619
Maubry94 <matthieu.aubry94370@gmail.com>
2024-04-05 16:28:34 +0200

    [ADD]: add social networks

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,11 +5,11 @@ import CornerBloc from "./corner-bloc";
 
 export default component$(() => {
   return (
-    <footer class="relative z-50 h-60 flex justify-center items-center bg-dark border-y border-grey">
+    <footer class="relative z-50 min-h-60 px-6 py-4 flex flex-col lg:flex-row gap-8 lg:gap-16 justify-center items-center bg-dark border-y border-grey">
       <CornerBloc positionX="left-0" positionY="top-0" />
       <CornerBloc positionX="right-0" positionY="top-0" />
       <nav>
-        <ul class="h-full flex flex-col lg:flex-row justify-start lg:justify-center items-center text-grey">
+        <ul class="h-full flex flex-col justify-start lg:justify-center items-center text-grey">
           <li class="mx-4">
             <Link prefetch href="/" class="tracking-[4px] hover:text-blue transition-colors">Accueil</Link>
           </li>
@@ -24,6 +24,34 @@ export default component$(() => {
 
           <li class="mx-4">
             <Link prefetch href="/embassy" class="tracking-[4px] hover:text-blue transition-colors">Ambassade</Link>
+          </li>
+        </ul>
+      </nav>
+      <nav>
+        <ul class="h-full flex flex-col justify-start lg:justify-center items-center text-grey">
+          <li class="mx-4 flex gap-1 items-center tracking-[4px] hover:text-blue transition-colors">
+            <a href="https://www.facebook.com/" target="_blank">
+              <span class="mdi mdi-facebook text-lg"></span>
+              Facebook
+            </a>
+          </li>
+          <li class="mx-4 flex gap-1 items-center tracking-[4px] hover:text-blue transition-colors">
+            <a href="https://twitter.com/" target="_blank">
+              <span class="mdi mdi-twitter text-lg"></span>
+              Twitter
+            </a>
+          </li>
+          <li class="mx-4 flex gap-1 items-center tracking-[4px] hover:text-blue transition-colors">
+            <a href="https://www.instagram.com/" target="_blank">
+              <span class="mdi mdi-instagram text-lg"></span>
+              Instagram
+            </a>
+          </li>
+          <li class="mx-4 flex gap-1 items-center tracking-[4px] hover:text-blue transition-colors">
+            <a href="https://www.youtube.com/" target="_blank">
+              <span class="mdi mdi-youtube text-lg"></span>
+              Youtube
+            </a>
           </li>
         </ul>
       </nav>
