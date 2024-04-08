f5ef607b72cea0c93a297df855568b96dbab997b
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2024-04-08 12:30:54 +0000

    [UPDATE]: metadatas

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -11,36 +11,24 @@ export const RouterHead = component$(() => {
   return (
     <>
       <title>{head.title}</title>
-
       <link rel="canonical" href={loc.url.href} />
-      <link rel="icon" type="image/x-icon" href="/images/favicons/favicon.ico" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
-      <link rel="mask-icon" href="/images/favicons/safari-pinned-tab.svg" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content={head.meta.find(m => m.name === 'description')?.content} />
-      <meta name="keywords" content="Zorglux" />
-      <meta name="theme-color" content="#080c0f" />
-      <meta name="msapplication-TileColor" content="#080c0f" />
-      <meta name="robots" content="index, follow" />
 
       {/* Open Graph / Facebook */}
+      <meta property="og:locale" content="fr_FR" />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://zorglux.fr/" />
-      <meta property="og:title" content="Zorglux - Le peuple suprême" />
+      <meta property="og:url" content={loc.url.href} />
+      <meta property="og:title" content={head.title} />
+      <meta property="og:site_name" content="Zorglux" />
       <meta property="og:description" content={head.meta.find(m => m.name === 'description')?.content} />
       <meta property="og:image" content="/images/logo.svg" />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://zorglux.fr/" />
-      <meta property="twitter:title" content="Zorglux - Le peuple suprême" />
+      <meta property="twitter:url" content={loc.url.href} />
+      <meta property="twitter:title" content={head.title} />
       <meta property="twitter:description" content={head.meta.find(m => m.name === 'description')?.content} />
       <meta property="twitter:image" content="/images/logo.svg" />
-
-      {/* Global style */}
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@7.4.47/css/materialdesignicons.min.css" />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
--- src/root.tsx
+++ src/root.tsx
@@ -19,9 +19,20 @@ export default component$(() => {
   return (
     <QwikCityProvider>
       <head>
-        <meta charSet="utf-8" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
+        <meta name="keywords" content="Zorglux" />
+        <meta name="theme-color" content="#080c0f" />
+        <meta name="msapplication-TileColor" content="#080c0f" />
+        <link rel="icon" type="image/x-icon" href="/images/favicons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
+        <link rel="mask-icon" href="/images/favicons/safari-pinned-tab.svg" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@7.4.47/css/materialdesignicons.min.css" />
       </head>
       <body lang="fr">
         <RouterOutlet />
