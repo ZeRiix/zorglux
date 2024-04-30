6f0e7d6ce6797e45a3086248a3893dfe976779dd
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2024-04-30 11:20:34 +0000

    [UPDATE]: 404 page

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -2,9 +2,12 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { Link } from '@builder.io/qwik-city';
 
+import ImgZorgluxLost from '/images/zorglux/lost.png'
+
 export default component$(() => {
     return (
         <section class="max-w-7xl h-full flex grow flex-col justify-center items-center text-skyblue">
+            <img src={ImgZorgluxLost} alt="Zorglux Lost" width={240} height={240} class=" max-h-60" />
             <h1 class="mb-10 text-3xl lg:text-5xl text-center text-yellow">
                 Page non trouvée
             </h1>
